cf6159445e92a6bc55aa07baec9e02192cbaee40
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-06-10 11:18:58 -0300

    Finished Project

---
+++ js/game_station.js
@@ -0,0 +1,27 @@
+const game_container = document.querySelector('#game-container');
+const ball = document.querySelector('.ball');
+const points_element = document.querySelector('.points');
+
+const button = document.querySelector('#double');
+const price_element = document.querySelector('.price');
+
+let points = 0;
+let amount = 1;
+let price = 10;
+
+ball.addEventListener('click', () => {
+    points += amount;
+    points_element.textContent = `Pontos: ${points}`;
+});
+
+ball.addEventListener('click', () => {
+    if (points === price) {
+        price *= 2;
+        price_element.textContent `Preço: ${price} pontos`;
+
+        amount *= 2;
+
+        points -= price;
+        points_element.textContent = `Pontos: ${points}`;
+    }
+});
---
+++ js/login.js
@@ -0,0 +1,10 @@
+function loginWithGitHub() {
+    window.location.href = 'https://github.com/login/oauth/authorize?client_id=5bbed63eb706151d7222&scope=user';
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const error = urlParams.get('error');
+
+if(error === 'access_denied') {
+    window.location.href = 'index.html';
+}
