cf6b2da2cdce69c27c99d0166d9767afdc253fab
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-06-10 14:00:00 -0300

    Final Project

--- mocks/questions.js
+++ mocks/questions.js
@@ -1,108 +1,108 @@
 export default [
     {
-        imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.worldatlas.com%2Fimg%2Fflag%2Fcd-flag.jpg&f=1&nofb=1&ipt=be5a722e93254a884f299d0f88c7fded20310a6f4d112042d4ceccd46a54658c&ipo=images",
-        question: "Qual é esse pais?",
+        imageUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46353/question-mark-emoji-clipart-md.png",
+        question: "Qual paradigma de programação visa resolver problemas de forma paralela, distribuindo tarefas entre vários processadores ou computadores?",
         answers: [
-            {option: "República do Congo", correct:false},
-            {option: "República demorcrátoca do Congo", correct:true},
-            {option: "República Dominicana", correct:false},
+            {option: "Programação orientada a objeto", correct:false},
+            {option: "Programação concorrente", correct:true},
+            {option: "Programação funcional", correct:false},
         ],
     },
 
     {
-        imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.britannica.com%2F77%2F7177-050-246CAF00%2FFlag-Burundi.jpg&f=1&nofb=1&ipt=f5b624e2e3538855cc103bff78f9f443e329ab182e2b81206f06b2d541711ed3&ipo=images",
-        question: "Qual é esse pais?",
+        imageUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46353/question-mark-emoji-clipart-md.png",
+        question: "Qual linguagem de programação é considerada a primeira linguagem de alto nível?",
         answers: [
-            {option: "Eritreia", correct:false},
-            {option: "Jamaica", correct:false},
-            {option: "Burundi", correct:true},
+            {option: "Ada", correct:false},
+            {option: "Plankalkül", correct:false},
+            {option: "FORTRAN", correct:true},
         ],
     },
     
     {
-        imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fflagpedia.net%2Fdata%2Fflags%2Fw1600%2Frw.png&f=1&nofb=1&ipt=279fdef795c3ad3e1c995709e27d468f2023ddd6335d395f07afa926977984b6&ipo=images",
-        question: "Qual é esse pais?",
+        imageUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46353/question-mark-emoji-clipart-md.png",
+        question: "Qual linguagem de programação foi criada para atender às demandas da inteligência artificial e do aprendizado de máquina, combinando expressividade e poder computacional?",
         answers: [
-            {option: "Palau", correct:false},
-            {option: "Ruanda", correct:true},
-            {option: "Macedónia do Norte", correct:false},
+            {option: "R", correct:false},
+            {option: "Python", correct:true},
+            {option: "Prolog", correct:false},
         ],
         
     },
 
     {
-        imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F3%2F38%2FFlag_of_Tanzania.svg%2F1200px-Flag_of_Tanzania.svg.png&f=1&nofb=1&ipt=826587f2d005701229ca1e4ff4aa7917856477228cd76a71ac8c739e7965c986&ipo=images",
-        question: "Qual é esse pais?",
+        imageUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46353/question-mark-emoji-clipart-md.png",
+        question: "Qual foi a principal contribuição de Ada Lovelace para a programação?",
         answers: [
-            {option: "Tanzânia", correct:true},
-            {option: "Timor Leste", correct:false},
-            {option: "Trindade e Tobago", correct:false},
+            {option: "Escreveu o primeiro algoritmo para uma máquina de computação.", correct:true},
+            {option: "Criou a primeira linguagem de programação.", correct:false},
+            {option: "Popularizou o uso de cartões perfurados para entrada de dados.", correct:false},
         ],
         
     },
 
     {
-        imageUrl: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fflags.fmcdn.net%2Fdata%2Fflags%2Fw1160%2Fvu.png&f=1&nofb=1&ipt=8ea1e827f35801944411b13dbaafd18c1abf64c493d0582c3499344c40b52b36&ipo=images",
-        question: "Qual é esse pais?",
+        imageUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46353/question-mark-emoji-clipart-md.png",
+        question: "Qual paradigma de programação enfatiza a manipulação de dados e a transformação de funções em outras funções?",
         answers: [
-            {option: "Eritreia", correct:false},
-            {option: "Guiana", correct:false},
-            {option: "Vanuatu", correct:true},
+            {option: "Programação orientada a objeto", correct:false},
+            {option: "Programação imperativa", correct:false},
+            {option: "Programação funcional", correct:true},
         ],
         
     },
 
     {
-        imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fflagsweb.com%2Fimages%2FPNG%2FFlag_of_Seychelles.png&f=1&nofb=1&ipt=63be07da53056d49b4bba4d65140d621ea5452fac47d46f4a63aa939da96a761&ipo=images",
-        question: "Qual é esse pais?",
+        imageUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46353/question-mark-emoji-clipart-md.png",
+        question: "Em qual década surgiram as primeiras linguagens de programação modernas, como FORTRAN e COBOL?",
         answers: [
-            {option: "Ilhas Marshal", correct:false},
-            {option: "Seychelles", correct:true},
-            {option: "Ilhas Salomão", correct:false},
+            {option: "Década de 1940", correct:false},
+            {option: "Década de 1950", correct:true},
+            {option: "Década de 1960", correct:false},
         ],
         
     },
 
     {
-        imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.medianigeria.com%2Fwp-content%2Fuploads%2F2018%2F02%2FPhilippines-Flag-1067x534.png&f=1&nofb=1&ipt=5fe5b27fd6ce168a05aeec0b3a825f6d881db6feef6e28a467185f15e5273732&ipo=images",
-        question: "Qual é esse pais?",
+        imageUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46353/question-mark-emoji-clipart-md.png",
+        question: "Qual linguagem de script foi criada para adicionar interatividade a páginas da web e se tornou essencial para o desenvolvimento web moderno?",
         answers: [
-            {option: "Filipinas", correct:true},
-            {option: "Chile", correct:false},
-            {option: "Chéquia", correct:false},
+            {option: "JavaScript", correct:true},
+            {option: "C#", correct:false},
+            {option: "Python", correct:false},
         ],
         
     },
 
     {
-        imageUrl: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fs1.bwallpapers.com%2Fwallpapers%2F2014%2F05%2F29%2Fnicaragua-flag_121347854.jpg&f=1&nofb=1&ipt=6ed3272728256328752cf314d4fd9ecdbc2a5100290aaa5216d037d8ca73a343&ipo=images",
-        question: "Qual é esse pais?",
+        imageUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46353/question-mark-emoji-clipart-md.png",
+        question: "Qual paradigma de programação foi fundamental para o desenvolvimento de interfaces gráficas e sistemas orientados a objetos?",
         answers: [
-            {option: "Nicarágua", correct:true},
-            {option: "Micronésia", correct:false},
-            {option: "Somália", correct:false},
+            {option: "Programação orientada a objetos", correct:true},
+            {option: "Programação funcional", correct:false},
+            {option: "Programação estruturada", correct:false},
         ],
         
     },
 
     {
-        imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpixelz.cc%2Fwp-content%2Fuploads%2F2018%2F11%2Fkiribati-flag-uhd-4k-wallpaper.jpg&f=1&nofb=1&ipt=6af857ec575dd1f4cc022f9a192264f0dfdf48f17000c62015bca9accb66c140&ipo=images",
-        question: "Qual é esse pais?",
+        imageUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46353/question-mark-emoji-clipart-md.png",
+        question: "Qual década foi marcada pelo desenvolvimento de linguagens como C, C++ e Java, que se tornaram populares para programação de sistemas e aplicações?",
         answers: [
-            {option: "Jordânia", correct:false},
-            {option: "Gâmbia", correct:false},
-            {option: "Kiribati", correct:true},
+            {option: "Década de 1970", correct:false},
+            {option: "Década de 1990", correct:false},
+            {option: "Década de 1980", correct:true},
         ],
         
     },
 
     {
-        imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.infoescola.com%2Fwp-content%2Fuploads%2F2017%2F04%2Fgabao-bandeira.png&f=1&nofb=1&ipt=cd8ea53b70a9bf6f0f58efb98e04f69a3c120d1ddb4b89844e8a84afeba52292&ipo=images",
-        question: "Qual é esse pais?",
+        imageUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46353/question-mark-emoji-clipart-md.png",
+        question: "Qual linguagem de programação foi criada para facilitar a manipulação de listas e estruturas de dados em árvores?",
         answers: [
-            {option: "Gabão", correct:true},
-            {option: "Ucrânia", correct:false},
-            {option: "Etiópia", correct:false},
+            {option: "LISP", correct:true},
+            {option: "C", correct:false},
+            {option: "Java", correct:false},
         ],
         
     },
